89228fd9948b3bfb80dc84bad0d8a65162cfcee6
Wilmer Gulcochia <karlwgs1989@gmail.com>
2024-10-14 18:01:09 -0500

    se creó una ArrayList "listaDeArtistas" y se ordenó alfabeticamente con el .sort(), también se ordenó "lista" de películas y series.

--- src/com/alura/screenmatch/principal/PrincipalConListas.java
+++ src/com/alura/screenmatch/principal/PrincipalConListas.java
@@ -5,12 +5,16 @@ import com.alura.screenmatch.modelos.Serie;
 import com.alura.screenmatch.modelos.Titulo;
 
 import java.util.ArrayList;
+import java.util.Collections;
 
 public class PrincipalConListas {
     public static void main(String[] args) {
         Pelicula miPelicula = new Pelicula("Encanto", 2021);
+        miPelicula.evalua(6);
         Pelicula otraPelicula = new Pelicula("Avatar", 2023);
+        otraPelicula.evalua(10);
         var peliculaDeWigusa = new Pelicula("Avengers", 2019);
+        peliculaDeWigusa.evalua(10);
         Serie lost = new Serie("Lost", 2000);
 
         ArrayList<Titulo> lista = new ArrayList<>();
@@ -21,9 +25,21 @@ public class PrincipalConListas {
 
         for (Titulo item : lista) {
             System.out.println(item);
-            if (item instanceof Pelicula pelicula) {
-                System.out.println(pelicula.getClasificacion());
+            if (item instanceof Pelicula pelicula && pelicula.getClasificacion() < 2){
+                System.out.println("Clasificación: " + pelicula.getClasificacion());
             }
         }
+
+        ArrayList<String> listaDeArtistas = new ArrayList<>();
+        listaDeArtistas.add("Penélope Cruz");
+        listaDeArtistas.add("Antonio Banderas");
+        listaDeArtistas.add("Ricardo Darin");
+        System.out.println("Lista de artistas no ordenada: " + listaDeArtistas);
+
+        Collections.sort(listaDeArtistas);
+        System.out.println("Lista de artistas ordenada: " + listaDeArtistas);
+
+        Collections.sort(lista);
+        System.out.println("Lista de títulos ordenados: " + lista);
     }
 }
