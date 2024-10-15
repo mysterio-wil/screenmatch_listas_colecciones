815f1c2432e97aa10363d3d49ca5c53234a6e650
Wilmer Gulcochia <karlwgs1989@gmail.com>
2024-10-15 12:43:40 -0500

    Manejo de excepciones para distintos tipos de errores con try-catch.

--- src/com/alura/screenmatch/principal/PrincipalConBusqueda.java
+++ src/com/alura/screenmatch/principal/PrincipalConBusqueda.java
@@ -21,23 +21,35 @@ public class PrincipalConBusqueda {
 
         String direccion = "https://www.omdbapi.com/?t="+busqueda+"&apikey=66d123fa";
 
-        HttpClient client = HttpClient.newHttpClient();
-        HttpRequest request = HttpRequest.newBuilder()
-                .uri(URI.create(direccion))
-                .build();
+        try {
+            HttpClient client = HttpClient.newHttpClient();
+            HttpRequest request = HttpRequest.newBuilder()
+                    .uri(URI.create(direccion))
+                    .build();
 
-        HttpResponse<String> response = client
-                .send(request, HttpResponse.BodyHandlers.ofString());
+            HttpResponse<String> response = client
+                    .send(request, HttpResponse.BodyHandlers.ofString());
 
-        String json = response.body();
-        System.out.println(json);
+            String json = response.body();
+            System.out.println(json);
 
-        Gson gson = new GsonBuilder()
-                .setFieldNamingPolicy(FieldNamingPolicy.UPPER_CAMEL_CASE)
-                .create();
-        TituloOmdb miTituloOmdb = gson.fromJson(json, TituloOmdb.class);
-        System.out.println(miTituloOmdb);
-        Titulo miTitulo = new Titulo(miTituloOmdb);
-        System.out.println(miTitulo);
+            Gson gson = new GsonBuilder()
+                    .setFieldNamingPolicy(FieldNamingPolicy.UPPER_CAMEL_CASE)
+                    .create();
+            TituloOmdb miTituloOmdb = gson.fromJson(json, TituloOmdb.class);
+            System.out.println(miTituloOmdb);
+
+            Titulo miTitulo = new Titulo(miTituloOmdb);
+            System.out.println("Título ya convertido: " + miTitulo);
+        } catch (NumberFormatException e) {
+            System.out.println("Ocurrió un error: ");
+            System.out.println(e.getMessage());
+        } catch (IllegalArgumentException e) {
+            System.out.println("Error en la URI, verifique la dirección.");
+        } catch (Exception e) {
+            System.out.println("Ocurrió un error inesperado.");
+        }
+        System.out.println("Finalizó la ejecución del programa!");
+
     }
 }
