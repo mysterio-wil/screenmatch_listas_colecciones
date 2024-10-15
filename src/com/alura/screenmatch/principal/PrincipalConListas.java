40ed947f404ab4c176c8f604ca010f532908cf36
Wilmer Gulcochia <karlwgs1989@gmail.com>
2024-10-14 19:02:19 -0500

    Se ordenó la lista por orden de lanzamiento usando .sort(Comparator.comparing()

--- src/com/alura/screenmatch/principal/PrincipalConListas.java
+++ src/com/alura/screenmatch/principal/PrincipalConListas.java
@@ -6,6 +6,7 @@ import com.alura.screenmatch.modelos.Titulo;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Comparator;
 
 public class PrincipalConListas {
     public static void main(String[] args) {
@@ -41,5 +42,8 @@ public class PrincipalConListas {
 
         Collections.sort(lista);
         System.out.println("Lista de títulos ordenados: " + lista);
+
+        lista.sort(Comparator.comparing(Titulo::getFechaDeLanzamiento));
+        System.out.println("Lista ordenada por fecha: " + lista);
     }
 }
